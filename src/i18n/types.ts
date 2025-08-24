68c41167b8aaa6eede8ea4ab7d2e7f0b6d3a5715
Lorax34 <ivanalejandroperezjimenez#@gmail.com>
2025-08-23 23:54:55 -0400

    Primera version

---
+++ src/i18n/types.ts
@@ -0,0 +1,45 @@
+export interface ServiceType {
+    name: string;
+    price: number;
+}
+
+export interface ServiceSection {
+    title: string;
+    tipo1: ServiceType;
+    tipo2: ServiceType;
+    tipo3: ServiceType;
+    tipo4?: ServiceType;
+    tipo5?: ServiceType;
+}
+
+export interface MenuSection {
+    seccion1: ServiceSection;
+    seccion2: ServiceSection;
+    seccion3: ServiceSection;
+    seccion4: ServiceSection;
+    seccion5: ServiceSection;
+}
+
+export interface Translation {
+    info1: string;
+    textButton: string;
+    titleServicio: string;
+    menuseccion: MenuSection;
+    info2: string;
+    titleHorario: string;
+    semana1: string;
+    semana2: string;
+    horario: string;
+    titleUbicacion: string;
+    titleContacto: string;
+    phone: number;
+    direccion: string;
+}
+
+export interface Translations {
+    pt: Translation;
+    en: Translation;
+    es: Translation;
+}
+
+export type Language = keyof Translations;
