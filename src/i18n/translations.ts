ee896cc73a259ae226567e36342ae0731bda5e73
Lorax34 <ivanalejandroperezjimenez#@gmail.com>
2025-08-25 23:58:03 -0400

    Listo casi

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -1,307 +1,313 @@
 import type { Translations } from "./types";
 
 export const translations: Translations = {
-    pt: {
-        info1: "Bem-vinda ao meu pequeno universo dedicado à arte e saúde das unhas.",
-        textButton: "Marcar Hora",
-        titleServicio: "Meus serviços",
-        menuseccion: {
-            seccion1: {
-                title: "Tradicional",
-                tipo1: {
-                    name: "Manicure com verniz normal",
-                    price: 12.0,
-                },
-                tipo2: {
-                    name: "Pedicure simple",
-                    price: 15.0,
-                },
-                tipo3: {
-                    name: "Pedicure simple + lixamento",
-                    price: 18.0,
-                },
-            },
-            seccion2: {
-                title: "Verniz de Gel",
-                tipo1: {
-                    name: "Manicure gel",
-                    price: 15.0,
-                },
-                tipo2: {
-                    name: "Pedicure gel",
-                    price: 18.0,
-                },
-                tipo3: {
-                    name: "Pedicure gel + lixamento",
-                    price: 20.0,
-                },
-            },
-            seccion3: {
-                title: "Extensão",
-                tipo1: {
-                    name: "Curto",
-                    price: 25.0,
-                },
-                tipo2: {
-                    name: "Meio",
-                    price: 30.0,
-                },
-                tipo3: {
-                    name: "Long",
-                    price: 38.0,
-                },
-            },
-            seccion4: {
-                title: "Manutenção",
-                tipo1: {
-                    name: "Curto",
-                    price: 20.0,
-                },
-                tipo2: {
-                    name: "Meio",
-                    price: 25.0,
-                },
-                tipo3: {
-                    name: "Long",
-                    price: 35.0,
-                },
-            },
-            seccion5: {
-                title: "Extras",
-                tipo1: {
-                    name: "Unha caida",
-                    price: 2.0,
-                },
-                tipo2: {
-                    name: "Decoração",
-                    price: 5.0,
-                },
-                tipo3: {
-                    name: "Pedicure Homen",
-                    price: 20.0,
-                },
-                tipo4: {
-                    name: "Remoção Gel",
-                    price: 8.0,
-                },
-                tipo5: {
-                    name: "Reforço unha natural",
-                    price: 15.0,
-                },
-            },
+  pt: {
+    info1:
+      "Bem-vinda ao meu pequeno universo dedicado à arte e saúde das unhas.",
+    textButton: "Marcar Hora",
+    titleServicio: "Meus serviços",
+    menuseccion: {
+      seccion1: {
+        title: "Tradicional",
+        tipo1: {
+          name: "Manicure com verniz normal",
+          price: 12.0,
         },
-        info2: "A minha maior recompensa é o seu sorriso de satisfação ao ver o resultado final.",
-        gallery: "Galeria",
-        galleryDescription: "Descubra alguns dos meus melhores trabalhos",
-        titleHorario: "Horários",
-        semana1: "Segunda a sexta-feira ",
-        semana2: "Sabado e domingo (apenas com maração) ",
-        horario: "15:00 - 19:00",
-        titleUbicacion: "Localização",
-        titleContacto: "Contacto",
-        phone: 351925764850,
-        direccion: "Rua de Santo Antonio 115, Faro, Portugal",
-    },
-    en: {
-        info1: "Welcome to my little universe dedicated to the art and care of your nails.",
-        textButton: "Book Appointment",
-        titleServicio: "My Services",
-        menuseccion: {
-            seccion1: {
-                title: "Traditional",
-                tipo1: {
-                    name: "Manicure with regular polish",
-                    price: 12.0,
-                },
-                tipo2: {
-                    name: "Simple pedicure",
-                    price: 15.0,
-                },
-                tipo3: {
-                    name: "Simple pedicure + filing",
-                    price: 18.0,
-                },
-            },
-            seccion2: {
-                title: "Gel Polish",
-                tipo1: {
-                    name: "Gel manicure",
-                    price: 15.0,
-                },
-                tipo2: {
-                    name: "Gel pedicure",
-                    price: 18.0,
-                },
-                tipo3: {
-                    name: "Gel pedicure + filing",
-                    price: 20.0,
-                },
-            },
-            seccion3: {
-                title: "Extensions",
-                tipo1: {
-                    name: "Short",
-                    price: 25.0,
-                },
-                tipo2: {
-                    name: "Medium",
-                    price: 30.0,
-                },
-                tipo3: {
-                    name: "Long",
-                    price: 38.0,
-                },
-            },
-            seccion4: {
-                title: "Maintenance",
-                tipo1: {
-                    name: "Short",
-                    price: 20.0,
-                },
-                tipo2: {
-                    name: "Medium",
-                    price: 25.0,
-                },
-                tipo3: {
-                    name: "Long",
-                    price: 35.0,
-                },
-            },
-            seccion5: {
-                title: "Extras",
-                tipo1: {
-                    name: "Broken nail",
-                    price: 2.0,
-                },
-                tipo2: {
-                    name: "Decoration",
-                    price: 5.0,
-                },
-                tipo3: {
-                    name: "Men's pedicure",
-                    price: 20.0,
-                },
-                tipo4: {
-                    name: "Gel removal",
-                    price: 8.0,
-                },
-                tipo5: {
-                    name: "Natural nail reinforcement",
-                    price: 15.0,
-                },
-            },
+        tipo2: {
+          name: "Pedicure simple",
+          price: 15.0,
         },
-        info2: "My greatest reward is your smile of satisfaction when you see the final result.",
-        gallery: "Gallery",
-        galleryDescription: "Discover some of my best work",
-        titleHorario: "Schedule",
-        semana1: "Monday to Friday ",
-        semana2: "Saturday and Sunday (only by appointment) ",
-        horario: "15:00 - 19:00",
-        titleUbicacion: "Location",
-        titleContacto: "Contact",
-        phone: 351925764850,
-        direccion: "Rua de Santo Antonio 115, Faro, Portugal",
-    },
-    es: {
-        info1: "Bienvenidas a mi pequeño universo dedicado al arte y la salud de tus uñas.",
-        textButton: "Reservar Cita",
-        titleServicio: "Mis Servicios",
-        menuseccion: {
-            seccion1: {
-                title: "Tradicional",
-                tipo1: {
-                    name: "Manicura con esmalte normal",
-                    price: 12.0,
-                },
-                tipo2: {
-                    name: "Pedicura simple",
-                    price: 15.0,
-                },
-                tipo3: {
-                    name: "Pedicura simple + limado",
-                    price: 18.0,
-                },
-            },
-            seccion2: {
-                title: "Esmalte de Gel",
-                tipo1: {
-                    name: "Manicura gel",
-                    price: 15.0,
-                },
-                tipo2: {
-                    name: "Pedicura gel",
-                    price: 18.0,
-                },
-                tipo3: {
-                    name: "Pedicura gel + limado",
-                    price: 20.0,
-                },
-            },
-            seccion3: {
-                title: "Extensión",
-                tipo1: {
-                    name: "Cortas",
-                    price: 25.0,
-                },
-                tipo2: {
-                    name: "Medias",
-                    price: 30.0,
-                },
-                tipo3: {
-                    name: "Largas",
-                    price: 38.0,
-                },
-            },
-            seccion4: {
-                title: "Mantenimiento",
-                tipo1: {
-                    name: "Cortas",
-                    price: 20.0,
-                },
-                tipo2: {
-                    name: "Medias",
-                    price: 25.0,
-                },
-                tipo3: {
-                    name: "Largas",
-                    price: 35.0,
-                },
-            },
-            seccion5: {
-                title: "Extras",
-                tipo1: {
-                    name: "Uña rota",
-                    price: 2.0,
-                },
-                tipo2: {
-                    name: "Decoración",
-                    price: 5.0,
-                },
-                tipo3: {
-                    name: "Pedicura hombre",
-                    price: 20.0,
-                },
-                tipo4: {
-                    name: "Retirada gel",
-                    price: 8.0,
-                },
-                tipo5: {
-                    name: "Refuerzo uña natural",
-                    price: 15.0,
-                },
-            },
+        tipo3: {
+          name: "Pedicure simple + lixamento",
+          price: 18.0,
         },
-        info2: "Mi mayor recompensa es tu sonrisa de satisfacción al ver el resultado final.",
-        gallery: "Galería",
-        galleryDescription: "Descubre algunos de mis mejores trabajos",
-        titleHorario: "Horarios",
-        semana1: "Lunes a viernes ",
-        semana2: "Sábado y domingo (solo con cita previa)",
-        horario: "15:00 - 19:00",
-        titleUbicacion: "Ubicación",
-        titleContacto: "Contacto",
-        phone: 351925764850,
-        direccion: "Rua de Santo Antonio 115, Faro, Portugal",
+      },
+      seccion2: {
+        title: "Verniz de Gel",
+        tipo1: {
+          name: "Manicure gel",
+          price: 15.0,
+        },
+        tipo2: {
+          name: "Pedicure gel",
+          price: 18.0,
+        },
+        tipo3: {
+          name: "Pedicure gel + lixamento",
+          price: 20.0,
+        },
+      },
+      seccion3: {
+        title: "Extensão",
+        tipo1: {
+          name: "Curto",
+          price: 25.0,
+        },
+        tipo2: {
+          name: "Meio",
+          price: 30.0,
+        },
+        tipo3: {
+          name: "Long",
+          price: 38.0,
+        },
+      },
+      seccion4: {
+        title: "Manutenção",
+        tipo1: {
+          name: "Curto",
+          price: 20.0,
+        },
+        tipo2: {
+          name: "Meio",
+          price: 25.0,
+        },
+        tipo3: {
+          name: "Long",
+          price: 35.0,
+        },
+      },
+      seccion5: {
+        title: "Extras",
+        tipo1: {
+          name: "Unha caida",
+          price: 2.0,
+        },
+        tipo2: {
+          name: "Decoração",
+          price: 5.0,
+        },
+        tipo3: {
+          name: "Pedicure Homen",
+          price: 20.0,
+        },
+        tipo4: {
+          name: "Remoção Gel",
+          price: 8.0,
+        },
+        tipo5: {
+          name: "Reforço unha natural",
+          price: 15.0,
+        },
+      },
     },
+    info2:
+      "A minha maior recompensa é o seu sorriso de satisfação ao ver o resultado final.",
+    gallery: "Galeria",
+    galleryDescription: "Descubra alguns dos meus melhores trabalhos",
+    titleHorario: "Horários",
+    semana1: "Segunda a sexta-feira ",
+    semana2: "Sabado e domingo (apenas com maração) ",
+    horario: "15:00 - 19:00",
+    titleUbicacion: "Localização",
+    titleContacto: "Contacto",
+    phone: 351925764850,
+    direccion: "Rua de Santo Antonio 115, Faro, Portugal",
+  },
+  en: {
+    info1:
+      "Welcome to my little universe dedicated to the art and care of your nails.",
+    textButton: "Book Appointment",
+    titleServicio: "My Services",
+    menuseccion: {
+      seccion1: {
+        title: "Traditional",
+        tipo1: {
+          name: "Manicure with regular polish",
+          price: 12.0,
+        },
+        tipo2: {
+          name: "Simple pedicure",
+          price: 15.0,
+        },
+        tipo3: {
+          name: "Simple pedicure + filing",
+          price: 18.0,
+        },
+      },
+      seccion2: {
+        title: "Gel Polish",
+        tipo1: {
+          name: "Gel manicure",
+          price: 15.0,
+        },
+        tipo2: {
+          name: "Gel pedicure",
+          price: 18.0,
+        },
+        tipo3: {
+          name: "Gel pedicure + filing",
+          price: 20.0,
+        },
+      },
+      seccion3: {
+        title: "Extensions",
+        tipo1: {
+          name: "Short",
+          price: 25.0,
+        },
+        tipo2: {
+          name: "Medium",
+          price: 30.0,
+        },
+        tipo3: {
+          name: "Long",
+          price: 38.0,
+        },
+      },
+      seccion4: {
+        title: "Maintenance",
+        tipo1: {
+          name: "Short",
+          price: 20.0,
+        },
+        tipo2: {
+          name: "Medium",
+          price: 25.0,
+        },
+        tipo3: {
+          name: "Long",
+          price: 35.0,
+        },
+      },
+      seccion5: {
+        title: "Extras",
+        tipo1: {
+          name: "Broken nail",
+          price: 2.0,
+        },
+        tipo2: {
+          name: "Decoration",
+          price: 5.0,
+        },
+        tipo3: {
+          name: "Men's pedicure",
+          price: 20.0,
+        },
+        tipo4: {
+          name: "Gel removal",
+          price: 8.0,
+        },
+        tipo5: {
+          name: "Natural nail reinforcement",
+          price: 15.0,
+        },
+      },
+    },
+    info2:
+      "My greatest reward is your smile of satisfaction when you see the final result.",
+    gallery: "Gallery",
+    galleryDescription: "Discover some of my best work",
+    titleHorario: "Schedule",
+    semana1: "Monday to Friday ",
+    semana2: "Saturday and Sunday (only by appointment) ",
+    horario: "15:00 - 19:00",
+    titleUbicacion: "Location",
+    titleContacto: "Contact",
+    phone: 351925764850,
+    direccion: "Rua de Santo Antonio 115, Faro, Portugal",
+  },
+  es: {
+    info1:
+      "Bienvenidas a mi pequeño universo dedicado al arte y la salud de tus uñas.",
+    textButton: "Reservar Cita",
+    titleServicio: "Mis Servicios",
+    menuseccion: {
+      seccion1: {
+        title: "Tradicional",
+        tipo1: {
+          name: "Manicura con esmalte normal",
+          price: 12.0,
+        },
+        tipo2: {
+          name: "Pedicura simple",
+          price: 15.0,
+        },
+        tipo3: {
+          name: "Pedicura simple + limado",
+          price: 18.0,
+        },
+      },
+      seccion2: {
+        title: "Esmalte de Gel",
+        tipo1: {
+          name: "Manicura gel",
+          price: 15.0,
+        },
+        tipo2: {
+          name: "Pedicura gel",
+          price: 18.0,
+        },
+        tipo3: {
+          name: "Pedicura gel + limado",
+          price: 20.0,
+        },
+      },
+      seccion3: {
+        title: "Extensión",
+        tipo1: {
+          name: "Cortas",
+          price: 25.0,
+        },
+        tipo2: {
+          name: "Medias",
+          price: 30.0,
+        },
+        tipo3: {
+          name: "Largas",
+          price: 38.0,
+        },
+      },
+      seccion4: {
+        title: "Mantenimiento",
+        tipo1: {
+          name: "Cortas",
+          price: 20.0,
+        },
+        tipo2: {
+          name: "Medias",
+          price: 25.0,
+        },
+        tipo3: {
+          name: "Largas",
+          price: 35.0,
+        },
+      },
+      seccion5: {
+        title: "Extras",
+        tipo1: {
+          name: "Uña rota",
+          price: 2.0,
+        },
+        tipo2: {
+          name: "Decoración",
+          price: 5.0,
+        },
+        tipo3: {
+          name: "Pedicura hombre",
+          price: 20.0,
+        },
+        tipo4: {
+          name: "Retirada gel",
+          price: 8.0,
+        },
+        tipo5: {
+          name: "Refuerzo uña natural",
+          price: 15.0,
+        },
+      },
+    },
+    info2:
+      "Mi mayor recompensa es tu sonrisa de satisfacción al ver el resultado final.",
+    gallery: "Galería",
+    galleryDescription: "Descubre algunos de mis mejores trabajos",
+    titleHorario: "Horarios",
+    semana1: "Lunes a viernes ",
+    semana2: "Sábado y domingo (solo con cita previa)",
+    horario: "15:00 - 19:00",
+    titleUbicacion: "Ubicación",
+    titleContacto: "Contacto",
+    phone: 351925764850,
+    direccion: "Rua de Santo Antonio 115, Faro, Portugal",
+  },
 };
